dd2ae28193fa039224f5a0196e52f2e44b990aa0
Kuroo <dkuric20@gmail.com>
2018-06-04 16:38:12 +0200

    REPS loss + MLP model

---
+++ models/mlp.py
@@ -0,0 +1,46 @@
+import torch
+from torch import Tensor
+from torch.nn import functional as F
+
+class MLP(torch.nn.Module):
+    def __init__(self, layers, activation=F.relu):
+        """
+        Creates neural network structure with
+        :param layers: list with linear layer sizes (input_size, hid1, hid2 , hid3, ..., output_size)
+        """
+        super(MLP, self).__init__()
+        self.layers = []
+        self.activation = activation
+        for i in range(len(layers)-1):
+            self.layers.append(torch.nn.Linear(layers[i], layers[i+1]))
+
+    def forward(self, x):
+        """
+        :param x: input features
+        :return: output of the network
+        """
+
+        "Forward pass (layer + activation)"
+        for i in range(len(self.layers) - 1):
+            x = self.activation(self.layers[i](x))
+        "Last layer without activation (no output domain restriction)"
+        x = self.layers[len(self.layers) - 1](x)
+        return x
+
+    def cuda(self,device=None):
+        for l in self.layers:
+            l.cuda(device)
+        return self
+
+    def cpu(self):
+        for l in self.layers:
+            l.cpu()
+        return self
+
+if __name__ == '__main__':
+    test = MLP([1,3,6,3,1])
+    input = torch.rand(1)
+
+    a = test(input)
+
+    print(a)
---
+++ utils/loss_reps.py
@@ -0,0 +1,15 @@
+import torch
+
+def loss_reps(epsilon, eta, vs_curr, vs_next, rewards):
+    """
+
+    :param epsilon: epsilon hyperparameter
+    :param eta: value of parameter eta
+    :param vs_curr: tensor of values of current states
+    :param vs_next: tensor of values of next states after performing action
+    :param rewards: rewards obtained when performing actions in states vs_curr
+    :return: reps loss
+    """
+
+    #TODO might be numerically unstable (use logsumexp trick)
+    return epsilon * eta + eta * torch.log(torch.sum(torch.exp((rewards+vs_next-vs_curr)/eta))/vs_curr.size(0))
