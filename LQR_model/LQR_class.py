5884e2691be5f15b8c216ffe96bd54515edd2617
TomScheffers <tom@vd.nl>
2018-06-04 12:31:06 +0200

    LQR_new

--- LQR_model/LQR_class.py
+++ LQR_model/LQR_class.py
@@ -5,7 +5,7 @@ class LQR:
     This is the class for the Linear Quadratic Rewards. Upon initialization it initializes
     random values for the state transistions and rewards. Then the functions can be called using the Step and Reset functions.
     '''
-    def __init__(self):
+    def __init__(self, min, max):
         #Initialize parameters for transistions and rewards:
         self.C1 = random.random()
         self.C2 = random.random()
@@ -19,16 +19,14 @@ class LQR:
         #We calculate the new state using the parameters:
         self.state = self.C3 * self.state + self.C4 * action
         self.reward = - self.C1 * (self.state ** 2)  - self.C2 * (action ** 2)
-        
         return self.state, self.reward
 
     def reset(self, min, max):
-        self.state = random.uniform(-10, 10)
-        
+        self.state = random.uniform(min, max)
         return self.state
 
 #Initialize a environment
-env = LQR()
+env = LQR(-10, 10)
 
 #Get a batch of data for random samples
 batch_size = 10
@@ -36,7 +34,7 @@ batch_data = []
 for s in range(batch_size):
     #We save all data in dicts
     data = {}
-    data['start_state'] = env.state
+    data['prev_state'] = env.state
     data['action'] = random.uniform(-1,1)
     data['new_state'], data['reward'] = env.step(data['action']) 
 
