e2011401cbee627cc5f3fb4a22d57e51590e8d2b
Stefan Knegt <stefanknegt@gmail.com>
2018-06-14 13:53:29 +0200

    save and tested trained policy model

--- half_cheetah.py
+++ half_cheetah.py
@@ -2,6 +2,9 @@ from values.value_model import Value
 from policies.policy_model import Policy
 from controller import Controller
 from utils.check_env import environment_check
+import torch
+import gym
+from torch.autograd import Variable
 
 name = 'HalfCheetah-v2'
 state_dim, action_dim, action_min, action_max = environment_check(name)
@@ -11,4 +14,6 @@ policy_model = Policy(state_dim, action_dim, hidden_dim, mu_range=0.4, sigma_ran
 value_model = Value(state_dim, hidden_dim, value_range=1, eta=0.1, epsilon=0.5, lr=0.01)
 
 model = Controller(name, policy_model, value_model, action_min, action_max)
-model.train(iterations=1000, batch_size=5000, trajectories=100, epochs=5, history_size=5, verbose=1)
+model.train(iterations=5000, batch_size=100, trajectories=10, epochs=5, history_size=5, verbose=1)
+
+policy_model.save('test.pth')
