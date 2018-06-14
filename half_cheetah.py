951638b3843ffbb72499e6253501f51770ad394a
Tom <tom@vd.nl>
2018-06-14 13:00:12 +0200

    dumb_error

--- half_cheetah.py
+++ half_cheetah.py
@@ -1,7 +1,7 @@
-from values.values_model import Value
+from values.value_model import Value
 from policies.policy_model import Policy
 from controller import Controller
-from data.check_env import environment_check
+from utils.check_env import environment_check
 
 name = 'HalfCheetah-v2'
 state_dim, action_dim, action_min, action_max = environment_check(name)
@@ -11,4 +11,4 @@ policy_model = Policy(state_dim, action_dim, hidden_dim, mu_range=0.4, sigma_ran
 value_model = Value(state_dim, hidden_dim, value_range=1, eta=0.1, epsilon=0.5, lr=0.01)
 
 model = Controller(name, policy_model, value_model, action_min, action_max)
-model.train(iterations=1000, batch_size=5000, trajectories=5, epochs=5, history_size=5, verbose=1)
+model.train(iterations=1000, batch_size=5000, trajectories=100, epochs=5, history_size=5, verbose=1)
