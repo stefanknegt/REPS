75c2ac668233948259b1fd7618881574ff45722e
Kuroo <dkuric20@gmail.com>
2018-06-17 20:58:54 +0200

    Pendulum in pendulum

--- run/pendulum.py
+++ run/pendulum.py
@@ -14,8 +14,8 @@ hidden_dim = 20
 policy_model = MLPNormalPolicy([state_dim, 15, 45, 1], sigma=4, learning_rate=1e-3, act_bound=2, activation=F.tanh)
 value_model = MLPValue([state_dim, 15, 45, 1], learning_rate=1e-3, activation=F.tanh)
 
-model = Controller(name, policy_model, value_model, reset_prob=0.02, history_depth=1, verbose=True)
-model.set_seeds(42)
-model.train(exp_episodes=50, exp_timesteps=100, val_epochs=100, batch_size=64)
+model = Controller(name, policy_model, value_model, reset_prob=0.02, history_depth=1, verbose=True,)
+model.set_seeds(41)
+model.train(iterations=30, exp_episodes=20, exp_timesteps=250, val_epochs=500, pol_epochs=500, batch_size=64, val_iterations=1)
 
 policy_model.save('../run/' + name + '.pth')
